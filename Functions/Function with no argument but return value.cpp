542f6c449b36bb5602ad73dae0c1da5d7fd6aa3a
Harshit Singh Negi <142771674+demon2202@users.noreply.github.com>
2024-07-09 20:53:50 +0530

    Create Function with no argument but return value.cpp

---
+++ Functions/Function with no argument but return value.cpp
@@ -0,0 +1,24 @@
+//Program to add two numbers using functions
+
+#include<iostream>
+
+#include<conio.h>
+
+using namespace std;
+
+int add(void);
+
+int main()
+
+{
+
+int z;
+
+z=add();
+
+cout<<"sum of 2 numbers is"<<z;
+
+
+return 0;
+
+}
