1d37bd3b115419063f7919b9f711d6fea0e7df2d
Harshit Singh Negi <142771674+demon2202@users.noreply.github.com>
2024-07-09 20:52:57 +0530

    Create Function with argument but no return value.cpp

---
+++ Functions/Function with argument but no return value.cpp
@@ -0,0 +1,27 @@
+//Program to add two numbers using functions
+
+#include<iostream>
+
+#include<conio.h>
+
+using namespace std;
+
+void add(int,int);
+
+int main()
+
+{
+
+int a,b;
+
+cout<<"enter values of a and b"<<endl;
+
+cin>>a>>b;
+
+add(a,b);
+
+getch();
+
+return 0;
+
+}
