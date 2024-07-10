baef20907e21028984f1b278d5786818f4d107ab
Harshit Singh Negi <142771674+demon2202@users.noreply.github.com>
2024-07-10 21:28:24 +0530

    Create Parameterised Constructor.cpp

---
+++ Constructors/Parameterised Constructor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Creature
+{
+private:
+int yearOfBirth;
+public:
+// …
+Creature(int year)
+{
+yearOfBirth = year;
+}
+};
+int main()
+{
+creature obj (2022);
+return 0;
+}
