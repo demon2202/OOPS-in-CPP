f13cff10c3a280ba80fc9d4d6387199cf851769d
Harshit Singh Negi <142771674+demon2202@users.noreply.github.com>
2024-07-10 21:27:37 +0530

    Create Default Constructor.cpp

---
+++ Constructors/Default Constructor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class creature
+{
+private:
+int yearofBirth;
+public:
+creature()
+{
+Yearofbirth = 2001;
+cout<<“Contructor Called";
+}
+};
+int main()
+{
+creature obj;
+return 0;
+}
